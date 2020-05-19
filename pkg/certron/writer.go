f743ef0fed47140560d0b88b417d2ddacefdce9b
David N Salazar <david@salzr.com>
2020-05-18 23:38:24 -0400

    nit: fixes import grouping

--- pkg/certron/writer.go
+++ pkg/certron/writer.go
@@ -2,14 +2,14 @@ package certron
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
